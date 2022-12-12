3ffce0a0ea0f823e09f8b1f144b0d53269790aea
magizergi <magizergi19@gmail.com>
2022-12-12 09:54:57 -0800

    5-print_numbers2

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'the program's description'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int al;
+for (al = 0; al <= 9; al++)
+{
+printf("%d", al);
+}
+printf("\n");
+return (0);
+}
