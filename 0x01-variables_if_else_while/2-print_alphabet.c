f610c8adb39a469cf1e453248b85f0dd293f78eb
magizergi <magizergi19@gmail.com>
2022-12-12 03:32:26 -0800

    alphaif1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'the program's description'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (char alpha = 'a'; alpha = 'z'; alpha++)
+{
+putchar(alpha);
+}
+}
