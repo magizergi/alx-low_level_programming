56bb5c9ecd8cb86fc474d7633d0a11d88fe99c4a
magizergi <magizergi19@gmail.com>
2022-12-19 08:23:07 -0800

    pointer 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void reset_to_98(int *n)
+{
+*n=98;
+}
