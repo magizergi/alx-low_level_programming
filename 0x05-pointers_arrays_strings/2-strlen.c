4f53bad803bc96cf77d151beed9f3fb70c092777
magizergi <magizergi19@gmail.com>
2022-12-20 07:13:39 -0800

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _strlen -> function to swap two numbers
+* @s:value to be swaped
+* return 0
+* error -1
+*/
+int _strlen(char *s)
+{
+int len = 0;
+while (*s == "/0")
+{
+len = len + 1;
+}
