adde2f33122e6418cccd613f1a76529b1bf3ed29
magizergi <magizergi19@gmail.com>
2022-12-26 08:18:35 -0800

    ,space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'the program's description'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int al;
+for (al = 0; al <= 9; al++)
+{
+putchar('0' + al);
+putchar(' ');
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
