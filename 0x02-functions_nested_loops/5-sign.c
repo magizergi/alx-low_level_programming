9bc8ba61bb0f201a21a4033608e8fea8c4b60ef2
magizergi <magizergi19@gmail.com>
2022-12-27 08:01:14 -0800

    5sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_sign - Entry point
+ *@n:char that is checked if lower
+ *Description: 'the program's description'
+ *Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+else
+if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
