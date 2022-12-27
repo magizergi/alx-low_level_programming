df625b7c9a1afb499db815203c45a46e084b5313
magizergi <magizergi19@gmail.com>
2022-12-27 06:56:23 -0800

    alpha10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *print_alphabet - Entry point
+ *print_alphabet_x10 - Entry point
  *Description: 'the program's description'
  *Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char alpha;
 int  i;
