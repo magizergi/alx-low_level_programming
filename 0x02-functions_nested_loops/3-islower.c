31e29927d1bcebf41308adb16a4e8fa602a26a32
magizergi <magizergi19@gmail.com>
2022-12-27 07:32:07 -0800

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower - Entry point
+ *@c:char that is checked if lower
+ *Description: 'the program's description'
+ *Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
