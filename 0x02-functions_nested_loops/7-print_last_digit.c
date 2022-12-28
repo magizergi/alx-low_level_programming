be0166776a8597f0a81c0702405f3d3032329ead
magizergi <magizergi19@gmail.com>
2022-12-28 03:55:26 -0800

    hello

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_last_digit - Entry point
+ *@n:char that is checked if lower
+ *Description: 'the program's description'
+ *Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+n = n % 10;
+return (n);
+}
