af2135d1bb476ba48997dd50c5e8e632a69418df
magizergi <magizergi19@gmail.com>
2022-12-29 05:22:03 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *add - Entry point
+ *@x :first value
+ *@y :second value
+ * Description: 'the program's description'
+ *Return: Always 0 (Success)
+ */
+int add(int x, int y)
+{
+int m;
+m = x + y;
+if (m > 9)
+{
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+}
+else
+{
+_putchar(m + '0');
+}
+_putchar('\n');
+}
