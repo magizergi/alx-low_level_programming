eb1b46aa99cf3add092262a1031e4c0608cb715c
magizergi <magizergi19@gmail.com>
2022-12-29 06:10:25 -0800

    stoe

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,13 @@ void print_to_98(int n)
 int x;
 for (x = n; x <= 98; x++)
 {
-_putchar(x);
+if (x > 9)
+{
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+}
+else
+_putchar(x + '0');
 _putchar(',');
 _putchar(' ');
 }
