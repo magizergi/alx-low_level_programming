61a1ea22fee2c529da5aa5c3e7ea5c1c845b543b
magizergi <magizergi19@gmail.com>
2023-01-03 07:04:23 -0800

    copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ *_memcpy - writes the character c to stdout
+ *@dest:character
+ *@src:character
+ *@n:character
+ *Return:Memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ *_strchr - writes the character c to stdout
+ *@s:character
+ *@c:character
+ *Return : Memory
+ */
+ char *_strchr(char *s, char c)
+{
+
+}
