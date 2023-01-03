284e1f8323edfc71fd0f7d5ad3ecc0a814a451cd
magizergi <magizergi19@gmail.com>
2023-01-03 07:38:22 -0800

    string fider

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,9 +3,17 @@
  *_strchr - writes the character c to stdout
  *@s:character
  *@c:character
- *Return : Memory
+ *Return:Memory
  */
- char *_strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
-
+int a;
+for (a = 0 ; s[i] = '/0'; i++)
+{
+if (s[i] == c)
+{
+return (&s[i]);
+}
+}
+return (0);
 }
