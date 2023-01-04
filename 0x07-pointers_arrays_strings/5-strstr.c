b333875cfdc20e9497ee79d4e182bc864bc63bc1
magizergi <magizergi19@gmail.com>
2023-01-03 19:16:53 -0800

    chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+/**
+ *_strpbrk - writes the character c to stdout
+ *@s:character
+ *@accept:character
+ *Return:Memory
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+while (*s)
+{
+for (i = 0 ; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+return (s);
+}
+}
+s++;
+}
+return ('\0');
+}
